43bc9ac39f5dee202b4c5d4f160f4c2f9a560339
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-09-05 10:09:15 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,28 @@
-print("Hello MyFirstBot")
-print("New Line")
+#1394735953:AAFaF2VQ5pi-tg_2OpQF2iFR0AUVouZdZtM
+from telegram.ext import Updater
+updater = Updater(token='1394735953:AAFaF2VQ5pi-tg_2OpQF2iFR0AUVouZdZtM', use_context=True)
+
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
+updater.start_polling()
