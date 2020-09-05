4129c179804601832d005ec092197d654a767d0f
ParkerXChen <parkerxchen@gmail.com>
2020-09-05 10:56:26 -0400

    Add token file support

--- hello.py
+++ hello.py
@@ -1,27 +1,73 @@
+
 from telegram.ext import Updater
-updater = Updater(token='1394735953:AAFaF2VQ5pi-tg_2OpQF2iFR0AUVouZdZtM', use_context=True)
-
-dispatcher = updater.dispatcher
-
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+from telegram.ext import MessageHandler, Filters
+from telegram.ext import CommandHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler
+import os
+
+def read_file_as_str(file_path):
+    # 判断路径文件存在
+    if not os.path.isfile(file_path):
+        raise TypeError(file_path + " does not exist")
+
+    # 读取文件内容到all_the_text
+    all_the_text = open(file_path).read()
+    # print type(all_the_text)
+    return all_the_text
+
 
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-from telegram.ext import MessageHandler, Filters
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+TOKEN=read_file_as_str('TOKEN')
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
 
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
 updater.start_polling()
