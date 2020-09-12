513a80148e5bf982b6d39def3ecc75966b42716d
ParkerXChen <parkerxchen@gmail.com>
2020-09-12 11:54:59 -0400

    UPDATED

--- ClasscraftRewardBot.py
+++ ClasscraftRewardBot.py
@@ -1,3 +1,5 @@
+import AddEXP
+import takeAwayEXP
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
@@ -6,16 +8,6 @@ import random
 
 def start(BotUpdate, context):
     context.bot.send_message(chat_id=BotUpdate.effective_chat.id, text="I'm a bot, please talk to me!")
-
-def ChangeEXP(BotUpdate, context):
-    Possibilitys = [
-    "haha! you lost 1000 EXP!",
-    "So lucky... You don't lose EXP...",
-    "You just lose 500 EXP, as normal.",
-    "THE GODS WILL DECIDE YOUR FATE, AND THEY HAVE DECIDED TO TAKE AWAY %s EXP" %(random.randint(500, 1500))]
-    #print (Possibilitys)
-    ChangeEXPmessage = Possibilitys[random.randint(0, 3)]    
-    context.bot.send_message(chat_id=BotUpdate.effective_chat.id, text = ChangeEXPmessage + " - By Parker")
 
 def echo(BotUpdate, context):
     msg = " We are in a %s chat. %s said %s ，your username is %s, your uid is %s，your system is in %s" %(
@@ -26,6 +18,8 @@ def echo(BotUpdate, context):
         BotUpdate.message.from_user.id,
         BotUpdate.message.from_user.language_code)
     context.bot.send_message(chat_id=BotUpdate.effective_chat.id, text= msg)
+
+
 
 def read_file_as_str(file_path):
     # 判断路径文件存在
@@ -47,8 +41,8 @@ dispatcher = updater.dispatcher
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-ChangeEXP_handler = CommandHandler('ChangeEXP', ChangeEXP)
-dispatcher.add_handler(ChangeEXP_handler)
+takeAwayEXP.addHandler(dispatcher)
+AddEXP.addHandler(dispatcher)
 
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
