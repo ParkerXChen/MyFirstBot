f35b45b8b33158ca90b1ab9f76ce4fa2185f1fc9
ParkerXChen <parkerxchen@gmail.com>
2020-10-04 20:25:32 -0400

    d

---
+++ MyBot.py
@@ -0,0 +1,42 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import Dispatcher,Updater
+import os
+import guess
+
+def start(update, context):
+    update.message.reply_text("HAHA WE STARTED")
+
+def message():
+    return """
+    You guessed a number from 0 - 100.
+/guessnumber Check your current status and get help.
+/guessnumber followed by a number 1-100 to guess that number.
+    """
+
+def read_file_as_str(file_path):
+    # 判断路径文件存在
+    if not os.path.isfile(file_path):
+        raise TypeError(file_path + " does not exist")
+
+    # 读取文件内容到all_the_text
+    all_the_text = open(file_path).read()
+    # print type(all_the_text)
+    return all_the_text
+
+TOKEN=read_file_as_str('GuessingBotToken')
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+message_handler = CommandHandler('message', message)
+dispatcher.add_handler(message_handler)
+
+guess.add_handler(dispatcher)
+
+guess.add_handler2(dispatcher)
+
+updater.start_polling()
---
+++ guess.py
@@ -0,0 +1,41 @@
+from telegram.ext import Dispatcher,Updater,CommandHandler
+import random
+
+Botsnumber = random.randint (1,100)
+NumberOfGuesses = 0
+
+def guessnumber(update, context):
+    global botsnumber
+    global NumberOfGuesses
+    if len(context.args) == 0:
+        update.message.reply_text(help())
+    else:
+        if (context.args[0]).isdigit():
+            number = int(context.args[0])
+            print (number)
+            print (botsnumber)
+            NumberOfGuesses += 1
+            if (number) == botsnumber:
+                update.message.reply_text("You guessed my number! It only took you %s tries!" %(NumberOfGuesses))
+                print (botsnumber)
+            else:
+                if number < botsnumber:
+                    update.message.reply_text("Nope! Too small! Try again!  Number of tries: %s" %(NumberOfGuesses))
+                else:
+                    update.message.reply_text("Nope! Too big! Try again! Number of tries: %s" %(NumberOfGuesses))
+        else: update.message.reply_text("Is that even a number?")
+
+def reset(update, context):
+    global botsnumber
+    global NumberOfGuesses
+    botsnumber = random.randint (1,100)
+    NumberOfGuesses = 0
+    update.message.reply_text("RESET COMPLETE")
+
+def add_handler(dp:Dispatcher):
+    guessnumber_handler = CommandHandler('guessnumber', guessnumber)
+    dp.add_handler(guessnumber_handler)
+
+def add_handler2(dp:Dispatcher):
+    reset_handler = CommandHandler('reset', reset)
+    dp.add_handler(reset_handler)
