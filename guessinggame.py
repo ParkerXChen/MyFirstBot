755fc579700a6caac69db35ad37805235b9e90fa
ParkerXChen <parkerxchen@gmail.com>
2020-10-24 10:15:13 -0400

    Changes made 10/24/2020
    Bug fizes

--- guessinggame.py
+++ guessinggame.py
@@ -2,45 +2,62 @@ from telegram.ext import Dispatcher,Updater,CommandHandler,CallbackQueryHandler
 from telegram import InlineKeyboardMarkup,InlineKeyboardButton
 import random
 
-answer = ""
+
+#Buttons:
 larger = InlineKeyboardButton('⬆️', callback_data='⬆️')
 smaller = InlineKeyboardButton('⬇️', callback_data='⬇️')
 
 kb = InlineKeyboardMarkup([[larger,smaller]])
 
+#Variables:
+answer = ""
+peopleinthegame = []
+
+number1 = random.randint(1,6)
+number2 = random.randint (1,6)
+number3 = random.randint (1,6)
+sumofthenumbers = number1 + number2 + number3
+message = 'I have generated three numbers from 1 - 6. \n \n If you think the sum of all three numbers is 10 or above, press the ⬆️ button.\n If you think the sum of all three numbers is 10 or below, press the ⬇️ button.\n \n If you guess right, you win a prize!\n\nThe people who are playing are:\n %s ' %(peopleinthegame) 
+games = {}
+
+def sendmessage(update, context):
+    global peopleinthegame
+    global kb
+    global message
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text= (message),reply_markup=kb)
+
 def playgame(update, context):
     global answer
-
-    number1 = random.randint(1,6)
-    number2 = random.randint (1,6)
-    number3 = random.randint (1,6)
-
-    sumofthenumbers = number1 + number2 + number3
 
     if sumofthenumbers < 10:
         answer = "⬇️"
     else:
         answer = "⬆️"
-    
-    print (sumofthenumbers)
-    update.message.reply_text ("""
-    I have generated three numbers from 1 - 6.
-    
-    If you think it is 10 or above, press the ⬆️ button.
-    If you think it is below 10, press the ⬇️ button.
 
-    If you guess right, you win a prize!
-    """ ,reply_markup=kb)
-
+    sendmessage(update, context)
+    print (answer)
 def buttonCallback(update, context):
     global answer
-
+    global sumofthenumbers
+    global message
     query = update.callback_query
-    print (query)
-    if query.data == answer:
-        print ("You got it correct!")
+
+    first_name = update.effective_user.first_name
+
+    if not first_name in peopleinthegame:
+        peopleinthegame.append(first_name)
+
+    if sumofthenumbers < 10:
+        answer = "⬇️"
     else:
-        print ("You got it wrong!")
+        answer = "⬆️"
+
+    if query.data == answer:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You got it correct!")
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You got it wrong!")
+
 def add_playgamehandler(dp:Dispatcher):
     playgame_handler = CommandHandler('playgame', playgame)
     dp.add_handler(playgame_handler)
