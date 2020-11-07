7fb78546778897f5f5ebc250a202adb18028b543
ParkerXChen <parkerxchen@gmail.com>
2020-11-07 10:36:34 -0500

    Updates Made 11/7/2020
    Bug fixes, Coin update (HUGE)

--- README.md
+++
@@ -1,5 +0,0 @@
-Dis be my bot
-
-The cost to use this bot is you have to give me a piece of cheese.
-
-Then you can use this bot however you like.
--- bot.py
+++ bot.py
@@ -1,40 +1,37 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
-from telegram.ext import Dispatcher,Updater
+from telegram.ext import MessageHandler, Filters
 import os
-import guess
-import food
-import guessinggame
+import rewards
+import guesscmd
 
 def start(update, context):
-    update.message.reply_text("HAHA WE STARTED")
+    print(update)
+    msg = "%s你好，你在%s说话"%(
+        update.message.from_user.first_name,
+        update.message.chat.type
+    )
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def read_file_as_str(file_path):
+    # 判断路径文件存在
     if not os.path.isfile(file_path):
         raise TypeError(file_path + " does not exist")
+
+    # 读取文件内容到all_the_text
     all_the_text = open(file_path).read()
+    # print type(all_the_text)
     return all_the_text
 
-TOKEN=read_file_as_str('GuessingBotToken')
+TOKEN=read_file_as_str('TOKEN')
 
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
+rewards.add_handler(dispatcher)
+guesscmd.add_handler(dispatcher)
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-
-guess.add_guesshandler(dispatcher)
-
-guess.add_resethandler(dispatcher)
-
-food.add_workhandler(dispatcher)
-
-food.add_talkhandler(dispatcher)
-
-food.add_eatcheesehandler(dispatcher)
-
-food.add_buycheesehandler(dispatcher)
-
-guessinggame.add_handler(dispatcher)
 
 updater.start_polling()
