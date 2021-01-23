c3c01e29780f0b5ed8741d8f414a4737f94f2e8e
ParkerXChen <parkerxchen@gmail.com>
2021-01-23 10:02:00 -0500

    zzz

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater,MessageHandler, Filters,CommandHandler
 from telegram import BotCommand
 import os
 import rewards
-import guesscmd,coins,votecmd, pinfocmd, ainfocmd, getmsgtypecmd, youtubemusic
+import guesscmd,coins,votecmd, pinfocmd, ainfocmd, getmsgtypecmd, youtubemusic, calander
 
 def start(update, context):
     print(update)
@@ -30,8 +30,9 @@ pinfocmd.add_pinfohandler(dispatcher)
 ainfocmd.add_ainfohandler(dispatcher)
 getmsgtypecmd.add_getmsgtypehandler(dispatcher)
 youtubemusic.add_ytmusichandler(dispatcher)
+calander.add_geteventshandler(dispatcher)
 
-commands = rewards.get_command() + guesscmd.get_command() +  coins.get_command() + votecmd.get_command() + pinfocmd.get_command() + ainfocmd.get_command() + getmsgtypecmd.get_command()
+commands = rewards.get_command() + guesscmd.get_command() +  coins.get_command() + votecmd.get_command() + pinfocmd.get_command() + ainfocmd.get_command() + getmsgtypecmd.get_command() 
 bot = updater.bot
 bot.set_my_commands(commands)
 
--- calander.py
+++ calander.py
@@ -1,6 +1,17 @@
+from telegram.ext import Dispatcher,CommandHandler
 from icalevents.icalevents import events
+from json import dumps
+import datetime
 
-url = "webcal://p72-caldav.icloud.com/published/2/NDE3OTkzMjk0NDE3OTkzMpSxb7ERH6yybgSlRn2nfyB4Hv7q5lfiDR7Fdx2tj3PeCWqnHVbkdy36ExtEYZ7PMeviqrlOuICN0CTECfxfDcw"
-es = events(url,fix_apple=True)
-for e in es:
-    print(e)
+def getevents(update,context):
+    url = "webcal://p60-caldav.icloud.com/published/2/MTMwMjQzNDk4NDEzMDI0M3sQtDCMMqfWL7VMca-urO1PHNC7k1S3xOJlT4pbFvB2zTOKsoMYKAaoX8kwofUBGi0yjak_7FqpXGZUZh5MhGY"
+    start = datetime.date.today() + datetime.timedelta(days=1)
+    end = datetime.date.today() + datetime.timedelta(days=1)
+    evs = events(url=url, start=start, end=end,fix_apple=True)
+    msg = ''
+    for i in evs:
+        msg += f'Event: {i.summary}\nStart date / time: {str(i.start)[0:10]} / {str(i.start)[0:-8]}\n\n'
+    update.message.reply_text(msg)
+
+def add_geteventshandler(dp:Dispatcher):
+    dp.add_handler(CommandHandler('getevents', getevents))
