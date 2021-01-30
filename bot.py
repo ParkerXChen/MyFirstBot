af01a3b4369db82437f67c77d70f2788957c5c0c
ParkerXChen <parkerxchen@gmail.com>
2021-01-30 11:16:09 -0500

    Epic Failing

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ from telegram.ext import Updater,MessageHandler, Filters,CommandHandler
 from telegram import BotCommand
 import os
 import rewards
-import guesscmd,coins,votecmd, pinfocmd, ainfocmd, getmsgtypecmd, youtubemusic, calander
+import guesscmd,coins,votecmd, pinfocmd, ainfocmd, getmsgtypecmd, youtubemusic, icalander
+from telegram.ext import Dispatcher, CallbackContext, CommandHandler
 
 def start(update, context):
     print(update)
@@ -30,13 +31,18 @@ pinfocmd.add_pinfohandler(dispatcher)
 ainfocmd.add_ainfohandler(dispatcher)
 getmsgtypecmd.add_getmsgtypehandler(dispatcher)
 youtubemusic.add_ytmusichandler(dispatcher)
-calander.add_geteventshandler(dispatcher)
+icalander.add_geteventshandler(dispatcher)
 
 commands = rewards.get_command() + guesscmd.get_command() +  coins.get_command() + votecmd.get_command() + pinfocmd.get_command() + ainfocmd.get_command() + getmsgtypecmd.get_command() 
 bot = updater.bot
 bot.set_my_commands(commands)
 
+job_queue = updater.job_queue
+
+icalander.run_repeating(job_queue)
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
+updater.idle()
---
+++ icalander.py
@@ -0,0 +1,27 @@
+from icalevents.icalevents import events
+from datetime import date, timedelta, time
+from telegram.ext import Dispatcher, CallbackContext, CommandHandler
+import pytz
+import coins
+
+def timer_callback(context:CallbackContext):
+    url = 'webcal://p60-caldav.icloud.com/published/2/MTMwMjQzNDk4NDEzMDI0M3sQtDCMMqfWL7VMca-urO1PHNC7k1S3xOJlT4pbFvB2zTOKsoMYKAaoX8kwofUBGi0yjak_7FqpXGZUZh5MhGY'
+    tommorow = date.today() + timedelta(days=1)
+    msg = ''
+    es = events(url,fix_apple=True,start=tommorow,end=tommorow)
+    for i in es:
+        msg += f'{i.summary}\nStart: {i.start}\nEnd:{i.end}\n{i.description}\n\n'
+    context.bot.send_message(chat_id=context.job.context,text=msg)
+
+def getevents(update,context):
+    chatid = update.message.chat_id
+    context.job_queue.run_repeating(timer_callback,5,context=chatid)
+
+def run_repeating(job_queue):
+    chatid = -1001366387264
+    job_queue.run_daily(timer_callback,
+                            time(hour=11, minute=53, tzinfo=pytz.timezone('US/Eastern')),
+                            days=(0, 1, 2, 3, 4, 5, 6), context=chatid)
+
+def add_geteventshandler(dp:Dispatcher):
+    dp.add_handler(CommandHandler('getevents', getevents))
